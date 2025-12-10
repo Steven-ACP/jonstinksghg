157af0277269de0200fe51857801057b159eccae
Steven-ACP <127SCastroPacheco@frhsd.com>
2025-12-10 13:12:09 -0500

    i love the state of isreal and benamin neayabu

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,6 @@
 
 let submitButton = document.getElementById("submitBtn");
 //
-//let raDio = document.querySelectorAll('[type="radio"]');
-//
-//let dreamPet = document.querySelectorAll('[type="checkbox"]');
-//
 submitButton.addEventListener("click", button);
 
 function button() {
@@ -14,16 +10,14 @@ function button() {
   let inputLocation = document.getElementById("locationInput");
   let inputPet = document.getElementById("petInput");
   let inputJob = document.getElementById("jobInput");
-  // let inputRadio = document.getElementById('[type="radio"]');
-  // let inputPet = document.getElementById('type="checkbox"]');
+  
   let value = inputName.value;
   let ageValue = inputAge.value;
   let locationValue = inputLocation.value;
   let petValue = inputPet.value;
   let jobValue = inputJob.value;
 
-  // let radioValue = inputRadio.value;
-  /// let petValue = inputPet.value;
+  
   document.getElementById("outPut").textContent =
     
   //  "Your Job(s)" +
